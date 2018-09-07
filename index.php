75d0302e9227678d6c78a29d8f08918e52bf5aab
amariniml <ayrton.marini@mercadolibre.com>
2018-09-06 22:17:52 -0300

    primer commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Prueba PHP</title>
+</head>
+<body>
+	<h1>Formulario Piola</h1>
+	<?php if(!isset($_POST["input1"])) { ?>
+		<form action="index.php" method="POST">
+			<h2>Llena los campos</h2>
+			<input name="input1" type="text">
+			<input name="input2" type="text">
+			<input name="input3" type="text">
+			<input name="input4" type="text">
+			<input value="enviar" type="submit">
+		</form>
+	<?php } else { ?>
+			<h1><?php echo $_POST["input1"]; ?></h1>
+
+	<?php } ?>
+</body>
+</html>
